0af39740741a703292365aa8c384390a9420efc1
Kenny Wagner <kennyacdc2@gmail.com>
2021-02-09 13:11:26 -0500

    Added initial work on multi-link support. Cleaned up files.

--- examples/ble_central/C&DH/pca10056/s140/ses/Output/Release/Obj/ble_app_uart_c_pca10056_s140/utf.d
+++
@@ -1,6 +0,0 @@
-Output/Release/Obj/ble_app_uart_c_pca10056_s140/utf.o: \
- C:\Nordic_Semi\WirelessInterconnect\external\utf_converter\utf.c \
- C:\Nordic_Semi\WirelessInterconnect\external\utf_converter\utf.h \
- C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.10a/include/stddef.h \
- C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.10a/include/__crossworks.h \
- C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.10a/include/stdint.h
--- examples/ble_central/ble_app_uart_c/pca10100/s122/ses/Output/Release/Obj/ble_app_uart_c_pca10100_s122/ses_startup_nrf_common.d
+++
@@ -1,2 +0,0 @@
-Output/Release/Obj/ble_app_uart_c_pca10100_s122/ses_startup_nrf_common.o: \
- C:\Nordic_Semi\WirelessInterconnect\modules\nrfx\mdk\ses_startup_nrf_common.s
